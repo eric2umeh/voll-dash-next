ad31bca1ba328b1c2fb8fa707bbc501656c8369e
osarinmwian <noel.osarinmwian@gmail.com>
2023-10-24 21:48:14 -0700

    Create folder

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -6,8 +6,6 @@ import LeftBar from '../../components/shared/LeftBar';
 import RightBar from '../../components/shared/RightBar';
 import BottomBar from '../../components/shared/BottomBar';
 import { Providers } from "../../redux/provider";
-import Login from '../auth/Login';
-import Verify from '../auth/Verify';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -20,16 +18,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Providers>
       <body className={inter.className}>
-        {/* <TopBar /> */}
-<Verify/>
-        {/* <main className="flex flex-row">
+        <TopBar />
+
+        <main className="flex flex-row">
           <LeftBar />
           <section className="main-container">
             <div className="w-full max-w-4xl">{children}</div>
-          </section> */}
+          </section>
           {/* @ts-ignore */}
-          {/* <RightBar /> */}
-        {/* </main> */}
+          <RightBar />
+        </main>
 
         <BottomBar />
       </body>
